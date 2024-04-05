0ce1b3997bb54450bc2af7501f317aad9c6d33a2
seop-h <jshwang9709@gmail.com>
2024-04-05 15:57:20 +0900

    feat: 차량 입출차 기능 추가 (#1)

--- src/main/java/com/capstone/parking/car/domain/Car.java
+++ src/main/java/com/capstone/parking/car/domain/Car.java
@@ -1,11 +1,57 @@
 package com.capstone.parking.car.domain;
 
+import lombok.Getter;
+
+import java.awt.Point;
+import java.util.List;
+
+@Getter
 public class Car {
 
-    private CarType type; //차종 (소형, 중형, 대형)
+    private final CarType type; //차종 (소형, 중형, 대형)
 
     private CarStatus status; //차량의 현재 상태 (입차 전, 입차 중, 입차 완료, 출차 전, 출차 중, 출차 완료)
 
     private MovingInfo movingInfo; //이동 관련 정보(이동 경로, 예상 소요 시간, 현재 위치 등)
+
+    public Car(CarType type, CarStatus status) {
+        this.type = type;
+        this.status = status;
+    }
+
+    /**
+     * 차량 입차 시도
+     * @param route 관제 시스템으로부터 제공받은 이동경로
+     * @return 제공받은 이동 경로, 예상 소요 시간, 현재 위치를 포함하는 이동 정보
+     */
+    public MovingInfo enter(List<Point> route) {
+        if (status != CarStatus.BEFORE_ENTER) {
+            //TODO 예외 처리
+            return null;
+        }
+        status = CarStatus.ENTERING;
+
+        //TODO 유효성 검증 : 제공받은 이동 경로가 해당 차종이 갈 수 있는 경로인지 확인
+        movingInfo = new MovingInfo(route);
+        return movingInfo;
+    }
+
+    /**
+     * 차량 출차 시도
+     * @param route 관제 시스템으로부터 제공받은 이동경로
+     * @return 제공받은 이동 경로, 예상 소요 시간, 현재 위치를 포함하는 이동 정보
+     */
+    public MovingInfo depart(List<Point> route) {
+        if (status != CarStatus.BEFORE_DEPARTURE) {
+            //TODO 예외 처리
+            return null;
+        }
+        status = CarStatus.DEPARTING;
+
+        //TODO 유효성 검증 : 제공받은 이동 경로가 해당 차종이 갈 수 있는 경로인지 확인
+        movingInfo = new MovingInfo(route);
+        return movingInfo;
+    }
+
 
 }
--- src/main/java/com/capstone/parking/car/domain/MovingInfo.java
+++ src/main/java/com/capstone/parking/car/domain/MovingInfo.java
@@ -9,6 +9,32 @@ public class MovingInfo {
 
     private Integer expectedTime; //예상 소요 시간
 
-    private Point currentLocation; //현재 위치
+    private Point currentLocation; //현재 위치(TODO 포인트 하나로 할지, 겹치는 경우를 고려해서 두개로 할지)
 
+    public MovingInfo(List<Point> route) {
+        this.route = route;
+        this.currentLocation = route.get(0);
+        calculateExpectedTime(route);
+    }
+
+    //TODO route 로부터 예상 소요 시간 계산
+    private void calculateExpectedTime(List<Point> route) {
+
+    }
+
+    public void updateLocation(Point nextLocation) {
+        if (!isAdjacent(currentLocation, nextLocation)) {
+            //TODO 예외 발생
+        }
+        if (!route.contains(nextLocation)) {
+            //TODO 예외 발생
+        }
+
+        currentLocation = nextLocation;
+    }
+
+    private boolean isAdjacent(Point current, Point next) {
+        return (Math.abs(current.getX() - next.getX()) == 1 && current.getY() == next.getY())
+                || (Math.abs(current.getY() - next.getY()) == 1 && current.getX() == next.getX());
+    }
 }
