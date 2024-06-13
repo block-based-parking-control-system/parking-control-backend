8649feeb6838d742d9cd222c924cc463ab279ce0
seop-h <jshwang9709@gmail.com>
2024-06-14 04:59:13 +0900

    feat: 두 칸 점유 기능 추가 (임시)

--- src/main/java/com/capstone/parking/config/rosbridge/topics/ROutTopic.java
+++ src/main/java/com/capstone/parking/config/rosbridge/topics/ROutTopic.java
@@ -1,5 +1,6 @@
 package com.capstone.parking.config.rosbridge.topics;
 
+import com.capstone.parking.controller.response.dto.EntranceInit;
 import com.capstone.parking.controller.response.dto.Location;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -11,6 +12,7 @@ import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 
 import java.awt.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @AllArgsConstructor
@@ -19,6 +21,8 @@ import java.util.List;
 @ToString
 @Slf4j
 public class ROutTopic {
+
+    private static final List<Point> entranceRoute = EntranceInit.create().getEntranceRoute();
 
     private String op;
 
@@ -38,12 +42,57 @@ public class ROutTopic {
     }
 
     public Location toLocation() {
-        int y = (115 - (int) this.getMsg().getPosition().getX()) / 10 + 1;
-        int x = (int) ((-1) * this.getMsg().getPosition().getY()) / 10;
+        double positionX = this.getMsg().getPosition().getX();
+        double positionY = this.getMsg().getPosition().getY();
 
-        return Location.of(
-                List.of(new Point(x, y))
-        );
+        int occupiedType = getOccupiedType(positionX, positionY);
+
+        int y = (115 - (int) positionX) / 10 + 1;
+        int x = (int) ((-1) * positionY) / 10;
+
+        List<Point> location = new ArrayList<>();
+        Point current = new Point(x, y);
+        location.add(current);
+
+        int idx = entranceRoute.indexOf(current);
+        if (occupiedType == 1) { //2칸 점유 (현재칸, 앞칸)
+            if (idx != entranceRoute.size() - 1) {
+                location.add(entranceRoute.get(idx + 1));
+            }
+        } else if (occupiedType == 2) { //2칸 점유 (현재칸, 뒷칸)
+            if (idx != 0) {
+                location.add(entranceRoute.get(idx - 1));
+            }
+        }
+
+        return Location.of(location);
+    }
+
+    private int getOccupiedType(double positionX, double positionY) {
+        double digitY = ((-1) * positionY) % 10;
+
+        if (digitY >= 0 && digitY < 2.5) {
+            log.info("[ROutTopic.toLocation()] occupiedType: {}, 앞칸 + 현재칸, x", 1);
+            return 1; //2칸 점유(현재칸, 앞칸)
+        }
+        if (digitY>= 7.5) {
+            log.info("[ROutTopic.toLocation()] occupiedType: {}, 뒷칸 + 현재칸, x", 2);
+            return 2; //2칸 점유(현재칸, 뒷칸)
+        }
+
+        double digitX = positionX % 10;
+
+        if (digitX >= 2.5 && digitX < 5) {
+            log.info("[ROutTopic.toLocation()] occupiedType: {}, 앞칸 + 현재칸, y", 1);
+            return 1; //2칸 점유(현재칸, 앞칸)
+        }
+        if (digitX >= 5 && digitX < 7.5) {
+            log.info("[ROutTopic.toLocation()] occupiedType: {}, 뒷칸 + 현재칸, y", 2);
+            return 2; //2칸 점유(현재칸, 뒷칸)
+        }
+
+        log.info("[ROutTopic.toLocation()] occupiedType: {}, 현재칸", 0);
+        return 0; //1칸 점유(현재칸)
     }
 
     @AllArgsConstructor
